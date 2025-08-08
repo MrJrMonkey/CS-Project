799cfe3dd5889e76534c8afe88175d077de81692
JrMonkey <152202611+MrJrMonkey@users.noreply.github.com>
2025-08-08 17:56:37 +0530

    Update stocks.sql

--- stocks.sql
+++ stocks.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE `items` (
+  `Prod_ID` int NOT NULL,
+  `Prod_Name` varchar(25) DEFAULT NULL,
+  `num_of_items` int DEFAULT NULL,
+  `MRP` int DEFAULT NULL,
+  PRIMARY KEY (`Prod_ID`)
